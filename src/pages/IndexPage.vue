5cb1e9df8a9d87e4f90299a8277e912ae124bde8
Marcelly Guimarães <souzaguimaraesarcelly@gmail.com>
2024-11-30 12:16:39 -0300

    Add: Criação da base da api

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,9 +1,9 @@
 <template>
   <q-page>
     <hero-section />
-    
+
     <stats-section :stats="stats" />
-    
+
     <about-section />
 
     <services-section />
@@ -11,9 +11,9 @@
     <stories-section />
 
     <members-section />
-    
+
     <news-section :news="news" />
-    
+
     <events-section />
 
     <photos-section />
@@ -21,48 +21,51 @@
 </template>
 
 <script setup>
-  import { ref } from 'vue'
-  import HeroSection from 'components/home/HeroComponent.vue'
-  import StatsSection from 'components/home/StatsComponent.vue'
-  import AboutSection from 'components/home/AboutComponent.vue'
-  import ServicesSection from 'components/home/ServicesComponent.vue'
-  import StoriesSection from 'components/home/StoriesComponent.vue'
-  import MembersSection from 'components/home/MembersComponent.vue'
-  import NewsSection from 'components/home/NewsComponent.vue'
-  import EventsSection from 'components/home/EventsComponent.vue'
-  import PhotosSection from 'components/home/PhotosComponent.vue'
+import { ref } from "vue";
+import HeroSection from "components/home/HeroComponent.vue";
+import StatsSection from "components/home/StatsComponent.vue";
+import AboutSection from "components/home/AboutComponent.vue";
+import ServicesSection from "components/home/ServicesComponent.vue";
+import StoriesSection from "components/home/StoriesComponent.vue";
+import MembersSection from "components/home/MembersComponent.vue";
+import NewsSection from "components/home/NewsComponent.vue";
+import EventsSection from "components/home/EventsComponent.vue";
+import PhotosSection from "components/home/PhotosComponent.vue";
 
-  defineOptions({
-    name: 'IndexPage'
-  })
+defineOptions({
+  name: "IndexPage",
+});
 
-  const stats = ref([
-    { value: '+35', label: 'Projetos realizados' },
-    { value: '+35', label: 'Projetos realizados' },
-    { value: '+135.958', label: 'Pessoas impactadas' }
-  ])
+const stats = ref([
+  { value: "+35", label: "Projetos realizados" },
+  { value: "+35", label: "Projetos realizados" },
+  { value: "+135.958", label: "Pessoas impactadas" },
+]);
 
-  const news = ref([
-    {
-      id: 1,
-      image: 'src/assets/imagens/noticias/noticia-1.png',
-      category: 'Categoria',
-      title: 'Lorem ipsum dolor sit amet consectetur',
-      description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
-    },
-    {
-      id: 2,
-      image: 'src/assets/imagens/noticias/noticia-2.png',
-      category: 'Categoria',
-      title: 'Lorem ipsum dolor sit amet consectetur',
-      description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
-    },
-    {
-      id: 3,
-      image: 'src/assets/imagens/noticias/noticia-3.png',
-      category: 'Categoria',
-      title: 'Lorem ipsum dolor sit amet consectetur',
-      description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
-    }
-  ])
+const news = ref([
+  {
+    id: 1,
+    image: "src/assets/imagens/noticias/noticia-1.png",
+    category: "Categoria",
+    title: "Lorem ipsum dolor sit amet consectetur",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+  },
+  {
+    id: 2,
+    image: "src/assets/imagens/noticias/noticia-2.png",
+    category: "Categoria",
+    title: "Lorem ipsum dolor sit amet consectetur",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+  },
+  {
+    id: 3,
+    image: "src/assets/imagens/noticias/noticia-3.png",
+    category: "Categoria",
+    title: "Lorem ipsum dolor sit amet consectetur",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+  },
+]);
 </script>
