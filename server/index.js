494ce0f9d5ce0861e3148f9cb626a6a8ba41b4d4
Marcelly Guimarães <souzaguimaraesarcelly@gmail.com>
2024-11-30 19:53:54 -0300

    Add: Ajuste na renderização local/produção

--- server/index.js
+++ server/index.js
@@ -9,7 +9,10 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middlewares
-app.use(cors());
+if (process.env.NODE_ENV !== "production") {
+  app.use(cors());
+}
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
--- src/boot/axios.js
+++ src/boot/axios.js
@@ -7,7 +7,14 @@ import axios from "axios";
 // good idea to move this instance creation inside of the
 // "export default () => {}" function below (which runs individually
 // for each client)
-const api = axios.create({ baseURL: "http://localhost:3000/api" });
+var baseURL;
+
+if (process.env.NODE_ENV === "production") {
+  baseURL = "https://teste-roos.onrender.com/api";
+} else {
+  baseURL = "http://localhost:3000/api";
+}
+const api = axios.create({ baseURL: baseURL });
 
 export default boot(({ app }) => {
   // for use inside Vue files (Options API) through this.$axios and this.$api
