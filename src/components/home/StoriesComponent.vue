d5a7deb6934fccb935ec39022d9c6d4ba7572eb6
Marcelly Guimarães <souzaguimaraesarcelly@gmail.com>
2024-11-30 17:59:59 -0300

    Add: Ajustes visuais nos stories

--- src/components/home/StoriesComponent.vue
+++ src/components/home/StoriesComponent.vue
@@ -1,18 +1,15 @@
-<!-- src/components/home/StoriesSection.vue -->
 <template>
   <section class="stories-section q-py-xl">
     <div class="container">
       <!-- First Story -->
       <div class="row q-col-gutter-xl items-center q-mb-xl">
         <div class="col-12 col-md-6">
-          <q-img
-            src="src/assets/imagens/story-1.png"
-            style="border-radius: 8px; height: 400px"
-          />
+          <q-img src="src/assets/imagens/story-1.png" class="story-image" />
         </div>
         <div class="col-12 col-md-6">
-          <p class="text-h5 text-weight-regular">
-            Lorem ipsum dolor sit amet consectetur. Tristique dignissim purus egestas nulla feugiat arcu tellus cum accumsan.
+          <p class="story-text">
+            Lorem ipsum dolor sit amet consectetur. Tristique dignissim purus
+            egestas nulla feugiat arcu tellus cum accumsan.
           </p>
         </div>
       </div>
@@ -20,25 +17,28 @@
       <!-- Second Story -->
       <div class="row q-col-gutter-xl items-center">
         <div class="col-12 col-md-6 gt-sm">
-          <p class="text-h5 text-weight-regular">
-            Lorem ipsum dolor sit amet consectetur. Tristique dignissim purus egestas nulla feugiat arcu tellus cum accumsan.
+          <p class="story-text">
+            Lorem ipsum dolor sit amet consectetur. Tristique dignissim purus
+            egestas nulla feugiat arcu tellus cum accumsan.
           </p>
         </div>
         <div class="col-12 col-md-6">
-          <q-img
-            src="src/assets/imagens/story-2.png"
-            style="border-radius: 8px; height: 400px"
-          />
+          <q-img src="src/assets/imagens/story-2.png" class="story-image" />
         </div>
-        <div class="col-12 col-md-6 lt-md q-mt-md">
-          <p class="text-h5 text-weight-regular">
-            Lorem ipsum dolor sit amet consectetur. Tristique dignissim purus egestas nulla feugiat arcu tellus cum accumsan.
+        <div class="col-12 col-md-6 lt-md">
+          <p class="story-text q-mt-md">
+            Lorem ipsum dolor sit amet consectetur. Tristique dignissim purus
+            egestas nulla feugiat arcu tellus cum accumsan.
           </p>
         </div>
       </div>
     </div>
   </section>
 </template>
+
+<script setup>
+// Component logic here if needed
+</script>
 
 <style scoped>
 .container {
@@ -47,15 +47,49 @@
   padding: 0 20px;
 }
 
+.story-image {
+  height: 450px;
+  width: 450px;
+  border-radius: 15px;
+  box-shadow: 0 4px 12px rgba(0, 0, 0, 0.1);
+}
+
+.story-text {
+  font-size: 33px;
+  line-height: 1.3;
+  font-weight: 600;
+  color: #333539;
+  margin: 0;
+}
+
 @media (max-width: 1023px) {
   .gt-sm {
     display: none;
+  }
+
+  .story-image {
+    height: 400px;
+  }
+
+  .story-text {
+    font-size: 24px;
+    text-align: center;
   }
 }
 
 @media (min-width: 1024px) {
   .lt-md {
     display: none;
+  }
+}
+
+@media (max-width: 599px) {
+  .story-image {
+    height: 300px;
+  }
+
+  .story-text {
+    font-size: 20px;
   }
 }
 </style>
